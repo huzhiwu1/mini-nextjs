b3ef7b7f75f49d160b88b546d60eb5f7f1d5c165
huzhiwu <15360829514@163.com>
2025-12-09 14:51:03 +0800

    chore: 实现自动监听文件变化打包并启动node服务

--- webpack.client.js
+++ webpack.client.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+  watch: true,
   entry: "./src/client/client.js",
   output: {
     path: path.resolve(__dirname, "public"),
--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 module.exports = {
   mode: "development",
+  watch: true,
   entry: "./src/react-ssr/server.js",
   target: "node",
   output: {
